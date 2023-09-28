aa47c6263ef0006857f5eb6250f2cc656b8f6325
Moritz Riede <moritz@mriede.com>
2023-09-29 01:09:57 +0200

    chore: import MidiRecorder

--- automidi/src/main.rs
+++ automidi/src/main.rs
@@ -2,8 +2,10 @@ use midir::MidiInput;
 
 pub mod midi_recorder;
 
+use self::midi_recorder::MidiRecorder;
+
 fn main() {
     let port_name = "CASIO USB-MIDI:CASIO USB-MIDI MIDI 1 24:0";
-    let recorder = midi_recorder::MidiRecorder::new(port_name);
+    let recorder = MidiRecorder::new(port_name);
     recorder.start_recording(MidiInput::new("AutoMIDI Listener").unwrap());
 }
