1fedda885a2a685b59a9101e4fe36511a4956ae2
Moritz Riede <moritz@mriede.com>
2023-09-28 23:38:31 +0200

    fix: compilation errors

--- nanomidi/src/main.rs
+++ nanomidi/src/main.rs
@@ -1,29 +1,29 @@
 use midir::{MidiInput, Ignore};
-use std::thread::sleep;
-use std::time::Duration;
 
 fn main() {
-    loop {
-        let midi_input = MidiInput::new("MIDI Listener").unwrap();
-        let port_name = "CASIO USB-MIDI MIDI 1";
+    let midi_input = MidiInput::new("MIDI Listener").unwrap();
+    let port_name = "CASIO USB-MIDI MIDI 1";
 
-        match midi_input.ports().iter().find(|port| port.name().unwrap().contains(port_name)) {
-            Some(port) => {
-                println!("{} found! Listening...", port.name().unwrap());
-                let in_port = midi_input.connect(port, "midir-read-input", |_, message, _| {
+    loop {
+        let ports = midi_input.ports();
+        
+        match ports.iter().find(|&&port| midi_input.port_name(port).unwrap().contains(port_name)) {
+            Some(&port) => {
+                println!("{} found! Listening...", midi_input.port_name(port).unwrap());
+
+                let in_port = midi_input.connect(port, "midir-read-input", move |_, message, _| {
                     println!("{:?}", message);
-                }, (), Ignore::None).unwrap();
+                }, ()).unwrap();
 
                 // Keep the program running to continue listening to MIDI messages
                 loop {
-                    sleep(Duration::from_secs(10));
+                    std::thread::sleep(std::time::Duration::from_secs(10));
                 }
             },
             None => {
                 println!("{} not found. Retrying in 10 seconds...", port_name);
-                sleep(Duration::from_secs(10));
+                std::thread::sleep(std::time::Duration::from_secs(10));
             }
         }
     }
 }
-
