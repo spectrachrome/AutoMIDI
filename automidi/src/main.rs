778285dc8e0e9b3af8d3dd1bbd93bfd18e30a99b
Moritz Riede <moritz@mriede.com>
2023-09-29 00:49:25 +0200

    feat: write incoming MIDI data to a file with a 20-second timeout

--- automidi/src/main.rs
+++ automidi/src/main.rs
@@ -1,30 +1,62 @@
-use midir::{MidiInput, Ignore};
-use std::{thread, time};
+use midir::MidiInput;
+use std::{sync::{Arc, Mutex}, thread, time::Duration, time::Instant};
+use midly::{Smf, Track, live::LiveEvent, MidiMessage};
 
 fn main() {
-    let mut midi_in = MidiInput::new("midir reading input").unwrap();
-    midi_in.ignore(Ignore::None);
+    let midi_input = MidiInput::new("MIDI Listener").unwrap();
+    let port_name = "CASIO USB-MIDI:CASIO USB-MIDI MIDI 1 24:0";
+
+    let buffer = Arc::new(Mutex::new(Vec::new()));
+    let last_received = Arc::new(Mutex::new(Instant::now()));
 
     loop {
-        let in_ports = midi_in.ports();
-        println!("Available ports:");
-        for port in &in_ports {
-            println!("{:?}", midi_in.port_name(port).unwrap());
-        }
+        let ports = midi_input.ports();
+        match ports.iter().find(|&port| midi_input.port_name(&port).unwrap() == port_name) {
+            Some(port) => {
+                println!("{} found! Listening...", midi_input.port_name(port).unwrap());
 
-        if let Some(in_port) = in_ports.iter().find(|p| midi_in.port_name(p).unwrap() == "CASIO USB-MIDI:CASIO USB-MIDI MIDI 1 24:0") {
-            println!("Found port: {:?}", midi_in.port_name(in_port).unwrap());
+                let buffer_clone = Arc::clone(&buffer);
+                let last_received_clone = Arc::clone(&last_received);
 
-            let _conn_in = midi_in.connect(in_port, "midir-read-input", move |_, message, _| {
-                println!("{:?}", message);
-            }, ()).unwrap();
+                let _conn_in = midi_input.connect(port, "midir-read-input", move |_, message, _| {
+                    let mut buffer = buffer_clone.lock().unwrap();
+                    if let Ok(event) = LiveEvent::parse(message) {
+                        if let LiveEvent::Midi { message, .. } = event {
+                            buffer.push(message);
+                        }
+                    }
+                    *last_received_clone.lock().unwrap() = Instant::now();
+                }, ()).unwrap();
 
-            // Prevent exiting immediately
-            thread::sleep(time::Duration::from_millis(10000));
-            break;
-        } else {
-            println!("Port not found, retrying in 5 seconds...");
-            thread::sleep(time::Duration::from_secs(5));
+                loop {
+                    thread::sleep(Duration::from_secs(1));
+
+                    let elapsed = last_received.lock().unwrap().elapsed();
+                    if elapsed >= Duration::from_secs(20) {
+                        let mut buffer = buffer.lock().unwrap();
+                        if !buffer.is_empty() {
+                            let timestamp = Instant::now().duration_since(Instant::now() - elapsed).as_secs();
+                            let filename = format!("{}.mid", timestamp);
+
+                            let track = Track::new();
+                            let smf = Smf {
+                                header: midly::Header {
+                                    format: midly::Format::SingleTrack,
+                                    timing: midly::Timing::Metrical(96.into()),
+                                },
+                                tracks: vec![track],
+                            };
+
+                            smf.save(&filename).unwrap();
+                            buffer.clear();
+                        }
+                    }
+                }
+            },
+            None => {
+                println!("{} not found. Retrying in 5 seconds...", port_name);
+                thread::sleep(Duration::from_secs(5));
+            }
         }
     }
 }
